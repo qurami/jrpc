f89f19acd15a7c0d1b9c1e5f5851c8dfc0c734a1
Marco Musella <mar.musella@gmail.com>
2017-03-01 14:30:49 +0100

    remove protocol for jsonecondable after review
    Signed-off-by: Marco Musella <mar.musella@gmail.com>

--- Example/Tests/JRPCClientTests.swift
+++ Example/Tests/JRPCClientTests.swift
@@ -12,7 +12,7 @@ import OHHTTPStubs
 
 class JRPCClientTests: XCTestCase {
     
-    let mockJRPCRequest = JRPCRequest(id: "1010", method: "mockMethod", namedParams: ["mock":"param"])
+    let mockJRPCRequest = JRPCRequest(id: "1010", method: "mockMethod", params: ["mock":"param"])
    
     let mockJRPCResponse = "{\"jsonrpc\":\"2.0\", \"id\":\"1010\",\"result\":{\"mock\":\"value\"},\"error\": null}"
     
--- jrpc/Classes/JRPCObjects.swift
+++ jrpc/Classes/JRPCObjects.swift
@@ -10,22 +10,10 @@ import Foundation
 
 
 
-/// A protocol to used to ensure that some given data
-/// is JSON encodable when passed into an array or a dictionary
-public protocol JSONEncodable{}
-
-extension String: JSONEncodable{}
-extension Int: JSONEncodable{}
-extension Double: JSONEncodable{}
-extension Bool: JSONEncodable{}
-
-
 /** A JSON RPC 2.0 request object, for further informations refer to http://www.jsonrpc.org/specification#request_object
  
     Since JSON RPC allows two different methods for passing parameters, named or positional, 
-    this struct exposes two different initializers and two attributes, 
-    specifically `namedParams` and `orderedParams`.
-    The two attributes are optional and can't both hold values at the same time.
+    this exposes a generic type Any params attribute.
 */
  public struct JRPCRequest{
 
@@ -33,10 +21,8 @@ extension Bool: JSONEncodable{}
     public let id: String
     /// the remote method to call on the JSONRPC server
     public let method: String
-    /// the list of ordered parameters for the remote method
-    public let orderedParams: Array<JSONEncodable>?
-    /// the dictionary of named parameters for the remote method
-    public let namedParams: Dictionary<String,JSONEncodable>?
+    /// the parameters for the remote method
+    public let params: Any?
     
     
     /**
@@ -46,39 +32,17 @@ extension Bool: JSONEncodable{}
      - Parameters:
         - id: the id of the JSONRPC request
         - method: the remote method to call
-        - namedParams: the dictionary of named parameters for the remote method
+        - params: the dictionary or array of named parameters, depending on the chosen param input method
      
      - Returns: a new JRPCRequest object with named parameters.
      */
-    public init(id:String, method: String, namedParams: Dictionary<String,JSONEncodable>) {
+    public init(id:String, method: String, params: Any?) {
         assert(!id.trimmingCharacters(in: CharacterSet.whitespaces).isEmpty)
         assert(!method.trimmingCharacters(in: CharacterSet.whitespaces).isEmpty)
         
         self.id = id
         self.method = method
-        self.namedParams = namedParams
-        self.orderedParams = nil
-    }
-    
-    /**
-     Initializes a new JRPCRequest object, using ordered parameters as
-     parameters passing method.
-     
-     - Parameters:
-     - id: the id of the JSONRPC request
-     - method: the remote method to call
-     - orderedParams: the list of ordered parameters for the remote method
-     
-     - Returns: a new JRPCRequest object with ordered parameters.
-     */
-    public init(id:String, method: String, orderedParams: Array<JSONEncodable>){
-        assert(!id.trimmingCharacters(in: CharacterSet.whitespaces).isEmpty)
-        assert(!method.trimmingCharacters(in: CharacterSet.whitespaces).isEmpty)
-        
-        self.id = id
-        self.method = method
-        self.orderedParams = orderedParams
-        self.namedParams = nil
+        self.params = params
     }
     
     /// Returns a JSON representation of the JRPCRequest, returns nil if the JSON encoding 
@@ -86,16 +50,8 @@ extension Bool: JSONEncodable{}
     public func toJSON() -> String?{
         
         var jsonData: Data
-        var params: Any
         
-        if let ordered = self.orderedParams{
-            params = ordered
-        } else {
-            params = self.namedParams!
-        }
-        
-        
-        let raw = ["jsonrpc":"2.0", "id": self.id, "method": self.method, "params":  params]
+        let raw = ["jsonrpc":"2.0", "id": self.id, "method": self.method, "params":  self.params]
         do{
             jsonData = try JSONSerialization.data(withJSONObject: raw, options: JSONSerialization.WritingOptions.prettyPrinted)
         } catch{
